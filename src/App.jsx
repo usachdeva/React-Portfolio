efa694916ffe6c82ab78531e21dc868610e27682
Udit Sachdeva <uditsachdeva5@gmail.com>
2024-07-20 12:17:32 -0400

    components structure created

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Project from "./components/Project";
+import Apps from "./components/Apps";
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Navbar />
+            <Apps />
+            <Apps />
+            <Apps />
+            <Apps />
+            <Project />
+            <Project />
+            <Project />
+        </div>
+    );
+}
+
+export default App;
